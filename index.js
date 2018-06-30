030c87a926eb18b64fafd798a2e143f14525b628
EnriqueCasGallardo <40712091+EnriqueCasGallardo@users.noreply.github.com>
2018-06-30 13:41:39 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var models = require("../models");
 var express = require("express");
 var router = express.Router();
-syslog2("ALERTA DE PRUEEEEEBAA");
+syslog2("ALERTA DE PRUEBA");
 /* GET home page. */
 router.get("/", function(req, res, next) {
   models.sequelize
